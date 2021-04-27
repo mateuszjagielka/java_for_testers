bebb20ce4e1077e4e5928ce51ed6e4dd0aea594d
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-04-27 19:27:47 +0200

    Couple of functions added

--- sandbox/src/main/java/pl/jagielka/mateusz/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/jagielka/mateusz/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package pl.jagielka.mateusz.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+		hello("world");
+		hello("Boczek");
 
-		System.out.println("Hello World!");
+		double l = 5;
+		System.out.println(area(l));
+		double b = 7;
+		System.out.println(area(l, b));
+
+	}
+	public static void hello(String somebody) {
+		System.out.println("Hello " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
