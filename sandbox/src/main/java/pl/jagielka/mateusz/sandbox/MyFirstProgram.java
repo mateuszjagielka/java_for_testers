c2e42dc2f0c9751202f49e8ee7f41e9256237e24
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-04-27 23:15:02 +0200

    Solution - exercise no. 2, point 4

--- sandbox/src/main/java/pl/jagielka/mateusz/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/jagielka/mateusz/sandbox/MyFirstProgram.java
@@ -5,12 +5,6 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		Point p1 = new Point(2, 3);
 		Point p2 = new Point(5, 7);
-		System.out.println("Distance between two points equals: " + distance(p1, p2));
-	}
-
-	public static double distance(Point p1, Point p2) {
-		double x = p1.x - p2.x;
-		double y = p1.y - p2.y;
-		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		System.out.println("Distance between two points equals: " + p1.distance(p2));
 	}
 }
