887ad15bfec2e056a72693a51951358ec107070d
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-05-11 10:14:11 +0200

    Exercise 7

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,11 @@
 package pl.jagielka.mateusz.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +16,23 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    } else if (browser.equals(BrowserType.EDGE)) {
+      wd = new EdgeDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package pl.jagielka.mateusz.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.jagielka.mateusz.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
