38494798e21e093dd105e7660aa6fc1464c2f62e
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-05-26 13:59:12 +0200

    Added sorting

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.jagielka.mateusz.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,6 +25,9 @@ public class GroupCreationTest extends TestBase {
 
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
