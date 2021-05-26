7188d27b99ed04c6e610c35ad600939d55bb1e01
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-05-26 13:44:54 +0200

    Added lambda

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.jagielka.mateusz.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,7 +14,7 @@ public class GroupCreationTest extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     GroupData group = new GroupData(
-            "test1",
+            "test5",
             "test2",
             "test3");
     app.getGroupHelper().createGroup(group);
@@ -23,13 +22,7 @@ public class GroupCreationTest extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
