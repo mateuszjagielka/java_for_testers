64fe12342010243ac897f6cb4b419a201f511e45
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-06-02 17:19:40 +0200

    Exercise 10

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/ContactCreationTest.java
@@ -1,20 +1,37 @@
 package pl.jagielka.mateusz.addressbook.tests;
 
+import net.bytebuddy.implementation.auxiliary.MethodCallProxy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.jagielka.mateusz.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class ContactCreationTest extends TestBase {
 
   @Test
   public void contactCreationTest() throws Exception {
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoAddNewContactPage();
-    app.getContactHelper().createContact(
-            new ContactData(
-                    "Adam",
-                    "Nowak",
-                    "569874125",
-                    "adam.nowak@secretmail.gov",
-                    "test1"),
-            true);
+    ContactData contact = new ContactData(
+            "Adam",
+            "Nowak",
+            "569874125",
+            "adam.nowak@secretmail.gov",
+            "test1");
+    app.getContactHelper().createContact(contact,true);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    int max = after.stream().max(byId).get().getId();
+    contact.setId(max);
+    before.add(contact);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
