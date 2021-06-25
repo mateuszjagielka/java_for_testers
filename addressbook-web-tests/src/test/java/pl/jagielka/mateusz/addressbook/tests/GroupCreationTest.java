fb7c3ec670c84b07961be9d527c5610dc4ee1595
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-06-25 14:05:52 +0200

    DataProvider added

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/GroupCreationTest.java
@@ -1,28 +1,42 @@
 package pl.jagielka.mateusz.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.jagielka.mateusz.addressbook.model.GroupData;
 import pl.jagielka.mateusz.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-  @Test (enabled = false, invocationCount = 1)
-  public void groupCreationTest() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+
+    return list.iterator();
+  }
+  @Test (dataProvider = "validGroups", enabled = true, invocationCount = 1)
+  public void groupCreationTest(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
 
     Groups after = app.group().all();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 
-  @Test (enabled = true)
+  @Test (enabled = false)
   public void badGroupCreationTest() throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
