f4e73d3fe486902a74d307c04746c01653f032c9
mateuszjagielka <mateusz.jagielka@gmail.com>
2021-06-28 13:21:39 +0200

    Object Relational Mapping (ORM) added

--- addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/pl/jagielka/mateusz/addressbook/tests/DbConnectionTest.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 import pl.jagielka.mateusz.addressbook.model.GroupData;
 import pl.jagielka.mateusz.addressbook.model.Groups;
 
-import java.nio.file.attribute.GroupPrincipal;
 import java.sql.*;
 
 public class DbConnectionTest {
